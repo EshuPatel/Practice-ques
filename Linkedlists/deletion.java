b67b994ceb4e5690e1904d509d505fb71292f603
Terryholic <eshupatel066@gmail.com>
2024-02-06 06:32:32 +0530

    deletion from sll

--- Linkedlists/deletion.java
+++ Linkedlists/deletion.java
@@ -1,32 +1,63 @@
 public class deletion extends basics {
 
-    Node head;
-    class Node{
-        String data;
-        Node nxt;
-
-        Node (String new_data){
-            this.data= new_data ;
-            this.nxt =null;
-        }
-    } 
-    
     // delete head
-    public void delBeg(){
-        Node temp=head.nxt;
-        head=temp;
+    void delBeg(){
+        if(head!=null){
+            head=head.nxt;
+            return;
+        }
+        else{
+            System.out.println("underflow");
+        }
     }
-    
+
+    // delete from end
+    void delEnd(){
+        if (head==null){
+            System.out.println("underflow");
+            return;
+        }
+        Node sl=head;
+        Node lnode=sl.nxt;
+        while(lnode!=null){
+            if(lnode.nxt==null){
+                sl.nxt=null;
+            }
+            sl=lnode;
+            lnode=lnode.nxt;
+        }
+    }
+    // from position
+    void delPos(int index,int size){
+        if(index>size){
+            System.out.println("length exceeded");
+        }
+        Node curr=head;
+        Node prev=head;
+        for(int i=0; i<=index; i++){
+            if(i==index){
+                curr.nxt=prev.nxt;
+                return;
+            }
+            curr=prev;
+            prev=prev.nxt;
+        }
+        
+        
+    }
     public static void main(String[] args) {
         deletion d =new deletion();
         d.insertBeg("Eshu");
         d.insertBeg("me");
         d.insertBeg("is");
         d.insertBeg("this");
-        System.out.println(d.head.data);
         d.printLs();
         d.delBeg();
         d.printLs();
-
+        d.delEnd();
+        d.printLs();
+        d.delPos(1,2);
+        d.printLs();
+    
     }
 }
